4a09e063ece256465efb4f626f4fc256afe8ce53
Kalin Kolev <kalink300@gmail.com>
2025-08-26 12:13:14 +0300

    feat: Added otaku scraping most recent releases

--- playwright/scraping_otakubg.py
+++ playwright/scraping_otakubg.py
@@ -11,10 +11,22 @@ with sync_playwright() as pw:
     page.click('span.ts-wpop-tab[data-range="monthly"]', timeout=10000)
 
     top_anime_list = page.locator('div.serieslist.pop.wpop.wpop-monthly div.leftseries a.series')
+    recent_releases = page.locator('span.ts-ajax-cache div.leftseries a.series ')
 
+    print("Top 5 most popular for the month:\n")
     for i in range(top_anime_list.count()):
+
+
         title = top_anime_list.nth(i).inner_text()
         url = top_anime_list.nth(i).get_attribute("href")
+
+        print(f"{i+1}. {title}: {url}")
+
+    print("\nMost Recent Releases:\n")
+    for i in range(recent_releases.count()):
+
+        title = recent_releases.nth(i).inner_text()
+        url = recent_releases.nth(i).get_attribute("href")
 
         print(f"{i+1}. {title}: {url}")
 
